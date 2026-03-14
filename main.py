0bc15adedc5a977d0034a5ff8346caf1321f083d
Cenliz <cenlipop2@gmail.com>
2026-03-14 23:07:59 +0100

    better SetSpeed, take angle 999 (and maybe every angles)

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 # in main.adofai, there is a "song" line with the name of the map
 # (maybe use image recognition on the bottom of the map once inside (the pause menue))
 
+
+# TODO: SetSpeed : prendre en compte les bpm (et plus seulement les mult)
 import keyboard,time
 
 class Map_part():
-    def __init__(self,name:str,angles:list[float],bpm:int,actions:list[tuple]|None) -> None:
+    def __init__(self,name:str,angles:list[float],bpm:float,actions:list[tuple]|None) -> None:
         self.__name = name
         self.__bpm = bpm
         self.__angles = angles
@@ -15,9 +17,14 @@ class Map_part():
     
     def get_angles(self)->list[float]:
         return self.__angles
+    def set_angle(self,angle:float,i:int)->None:
+        self.__angles[i] = angle
     
-    def get_bpm(self)->int:
+    def get_bpm(self)->float:
         return self.__bpm
+    
+    def set_bpm(self,bpm:float)->None:
+        self.__bpm = bpm
     
     def get_actions(self)->list[tuple]|None:
         return self.__actions
@@ -99,11 +106,16 @@ def scrap_actions(file:str)->list[str]|None:
                 floor_number = int(scraped_file[i][13:scraped_file[i].find(",")])
                 bpm = float(scraped_file[i][find_sub_str(scraped_file[i],",",3)+20:find_sub_str(scraped_file[i],",",4)])
                 bpm_mult = float(scraped_file[i][find_sub_str(scraped_file[i],",",4)+19:find_sub_str(scraped_file[i],",",5)])
-                scrap_list.append((type,floor_number,bpm,bpm_mult)) # (type, floor number, bpm, bpm mult)
+                scrap_list.append((type,floor_number,bpm,bpm_mult))
+            elif "\"PositionTrack\"" in scraped_file[i]:
+                type = "PositionTrack"
+                floor_number = int(scraped_file[i][13:scraped_file[i].find(",")])
+                rotation = int(scraped_file[i][scraped_file[i].find("rotation")+11:find_sub_str(scraped_file[i],",",7)])
+                scrap_list.append((type,floor_number,rotation))
     
     if len(scrap_list) == 0:
         return None
-    return scrap_list[2:]
+    return scrap_list[1:]
 
 def find_sub_str(string:str,sub_string:str,occurence:int=1)->int:
         if not(sub_string in string):
@@ -112,46 +124,58 @@ def find_sub_str(string:str,sub_string:str,occurence:int=1)->int:
             return string.find(sub_string)
         add = string.find(sub_string)+1
         return find_sub_str(string[add:],sub_string,occurence-1) + add
-#MODIF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-def calcul_timing(map_part:Map_part,current_tile:int,twirl:bool)->float: # "err code" 100: angle not supported
+
+def calcul_timing(map_part:Map_part,current_tile:int,twirl:bool)->float: # "err code" 10: angle not supported
     current_angle = map_part.get_angles()[current_tile - 1]
     next_angle = map_part.get_angles()[current_tile]
     if current_angle == next_angle:
-            return 1
+        return 1
+    if next_angle == 999:
+        return 999
     if twirl:
         if next_angle == current_angle - 45:
             return 0.75
-        elif next_angle == current_angle + 45:
+        elif next_angle == (current_angle + 45)%360:
             return 1.25
         elif next_angle == current_angle - 90:
             return 0.5
-        elif next_angle == current_angle + 90:
+        elif next_angle == (current_angle + 90)%360:
             return 1.5
         elif next_angle == current_angle - 135:
             return 0.25
-        elif next_angle == current_angle + 135:
+        elif next_angle == (current_angle + 135)%360:
             return 1.75
+        elif next_angle == current_angle - 270:
+            return 1.5
+        elif next_angle == (current_angle + 270)%360:
+            return 0.5
     #else
-    if next_angle == current_angle + 45:
+    if next_angle == (current_angle + 45)%360:
         return 0.75
     elif next_angle == current_angle - 45:
         return 1.25
-    elif next_angle == current_angle + 90:
+    elif next_angle == (current_angle + 90)%360:
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         return 0.5
     elif next_angle == current_angle - 90:
         return 1.5
-    elif next_angle == current_angle + 135:
+    elif next_angle == (current_angle + 135)%360:
         return 0.25
     elif next_angle == current_angle - 135:
         return 1.75
+    elif next_angle == current_angle - 270:
+        return 0.5
+    elif next_angle == (current_angle + 270)%360:
+        return 1.5
     
-    print("calcul_timing: err code 100")
-    return 100
+    print("calcul_timing: err code 10")
+    return 10
 
 # kawaii... = 2895342067
 path = "C:\\Program Files (x86)\\Steam\\steamapps\\Workshop\\Content\\977950\\" + "3018063128\\" # game path + map
 board = take_board(path)
-twirl = False
+twirl = True # 1st checkpoint
+time_calculated = False
 
 ###############################################################################
 for i in board:
@@ -160,26 +184,42 @@ for i in board:
 
 keyboard.wait('s') # start
 start = time.time()
-current_tile = 1 # manual start
+current_tile = 66 #1st checkpoint + manual start, 1 # manual start
+
 
 while True:
     if keyboard.is_pressed('f'): # fail safe
         break
-    timing = calcul_timing(board[-1],current_tile,twirl) # temp, main used
-    if time.time() - start >= (1/(board[-1].get_bpm()/60))*timing:
+    if  not time_calculated:
+        timing = calcul_timing(board[-1],current_tile,twirl) # temp, main used
+        if timing == 999:
+            timing = 1
+            current_tile +=1
+        time_calculated = True
+        timing = (1/(board[-1].get_bpm()/60))*timing
+        
+    
+    if time.time() - start >= timing*2: # 50% speed
         start = time.time()
         keyboard.press_and_release('j')
+        current_tile += 1
+        time_calculated = False
+        print(board[-1].get_angles()[current_tile-1],board[-1].get_angles()[current_tile], current_tile)
         for action in board[-1].get_actions():
             if action[1] == current_tile:
-                #aply effect
+                #apply effect
                 if action[0] == "Twirl":
-                    if twirl == True:
+                    if twirl:
                         twirl = False
                     else:
                         twirl = True
                 if action[0] == "SetSpeed":
-                    bpm = action[2] * action[3]
-                print(action)
+                    board[-1].set_bpm(board[-1].get_bpm() * action[3])
+                if action[0] == "PositionTrack":
+                    board[-1].set_angle(action[2],current_tile-1)
+                    
+                
+
     
-        current_tile += 1
+       
         
